2ee0d825966d0e44458ee08b8a49a97c98b8af16
Matthew Xu <matthew.xu@carel.locale>
2020-11-25 17:02:36 +0800

    Initial commit

---
+++ src/main/java/com/carel/sheetBroadcaster/SheetBroadcasterApplication.java
@@ -0,0 +1,13 @@
+package com.carel.sheetBroadcaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetBroadcasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetBroadcasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carel/sheetBroadcaster/SheetBroadcasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carel.sheetBroadcaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetBroadcasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
